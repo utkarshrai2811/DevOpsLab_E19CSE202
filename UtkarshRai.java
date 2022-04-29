40ee6f1fd9576951a347cb4777b0e61ca90d3c4c
Utkarsh Rai <53397945+utkarshrai2811@users.noreply.github.com>
2022-04-29 10:51:05 +0530

    Create UtkarshRai.java

---
+++ UtkarshRai.java
@@ -0,0 +1,14 @@
+public class UtkarshRai {
+
+    public static void main(String[] args) {
+        System.out.println("Subject: ENVIRONMENT AND SUSTAINABILITY      credits: 3");
+        System.out.println("Subject: Capstone Design      credits: 1");
+        System.out.println("Subject: SEMINAR ON SPECIAL TOPICS IN EMERGING AREAS      credits: 1");
+        System.out.println("Subject: Undergraduate Research in Computer Science      credits: 1");
+        System.out.println("Subject: HIGH PERFORMANCE COMPUTING      credits: 4");
+        System.out.println("Subject: Communication Skills for Global Professionals      credits: 3");
+        System.out.println("Subject: Software Project Management       credits: 3");
+        System.out.println("Subject: Career Skills for IT Companies      credits: 3");
+        System.out.println("Subject: DevOps Engineering Practice       credits: 3");
+    }
+}
